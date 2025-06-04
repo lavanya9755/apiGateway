171967fc44c67d8c9085425c0a1f2cb017af6eed
lavanya9755 <lavanyachawla24@gmail.com>
2025-06-04 23:43:48 +0530

    apiGateway Springboot

---
+++ src/main/java/com/gateway/apigateway/Routes/Routes.java
@@ -0,0 +1,22 @@
+package com.gateway.apigateway.Routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.function.HandlerFunction;
+// import org.springframework.web.servlet.function.RequestPredicate;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+    
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRouter(){
+        return GatewayRouterFunctions.route("productservice")
+                .route(RequestPredicates.path("/api/product"),HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+}
